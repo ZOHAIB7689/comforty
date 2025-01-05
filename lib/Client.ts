7a2d7b1f1528a211b8e00a466fc04e2e71424e9e
ZOHAIB7689 <bzohaib754@gmail.com>
2025-01-05 17:29:23 +0500

    main

--- lib/Client.ts
+++ lib/Client.ts
@@ -1,11 +1,8 @@
 import { createClient } from "next-sanity";
 
-
-
 export const client = createClient({
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_VERSION,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  token:process.env.SANITY_ACCESS_TOKEN,
-  useCdn:true
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2023-05-03",
+  useCdn: false, // Set to false for better reliability during build
 });
