24f1813ce8a561553e5f7a6086c7ae14e6a27e30
ZOHAIB7689 <youremail@example.com>
2025-01-09 19:41:15 +0500

    main

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,8 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
+import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-})
+  projectId: process.env.NEXT_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2023-05-03",
+  useCdn: false, // Set to false for better reliability during build
+});
