00fdc459a545fab02452d6932c9d45b5117ac935
ZOHAIB7689 <bzohaib754@gmail.com>
2025-01-14 12:53:59 +0500

    new commit

--- sanity/env.ts
+++ sanity/env.ts
@@ -8,8 +8,8 @@ export const dataset = assertValue(
 
 export const projectId = assertValue(
   process.env.NEXT_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+  "Missing environment variable: NEXT_SANITY_PROJECT_ID"
+);
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
